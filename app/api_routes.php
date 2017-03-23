f307f4bc1392c4108e584d0824939ccf8c69a445
Ju/TroisYaourts <julien@troisyaourts.com>
2017-03-23 18:25:01 +0100

    list action

--- app/api_routes.php
+++ app/api_routes.php
@@ -8,15 +8,32 @@ use Doctrine\DBAL\Query\QueryBuilder;
 
 
 $app->get('/', function(Request $request) use ($app){
-  
   $q = $request->query->get("q");
-
   return new JsonResponse(["ok" => $q], 200);
+});
+
+$app->get('/films', function(Request $request) use ($app){
+  
+  $limit = $request->query->get("limit", 10);
+  $page = $request->query->get("page", 1);
+  $offset = ($page - 1) * $limit;
+
+  $qb = new QueryBuilder($app['db']);
+  $qb->select("*")
+          ->from("film")
+          ->setMaxResults($limit)
+          ->setFirstResult($offset)
+          ->orderBy("annee_production", "DESC")
+          ;
+  $res = $qb->execute();
+  $films = $res->fetchAll();
+
+  return new JsonResponse($films, 200);
+
 });
 
 $app->get('/film/{filmid}', function(Request $request, $filmid) use ($app){
   
-
   $qb = new QueryBuilder($app['db']);
   $qb->select("*")
           ->from("films")
