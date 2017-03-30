e8fceecc39c0d0bda21fe5602f3728dc617ebe14
Ju/TroisYaourts <julien@troisyaourts.com>
2017-03-30 17:45:46 +0200

    client creation film

--- scripts/client.php
+++ scripts/client.php
@@ -14,23 +14,41 @@ $client = new Client(['base_uri' => "http://localhost:8000"]);
 // var_dump($body);
 
 
-$reponse = $client->request("GET", "/film/22");
-$film = json_decode($reponse->getBody());
-var_dump($film->duree_minutes);
+// $reponse = $client->request("GET", "/film/22");
+// $film = json_decode($reponse->getBody());
+// var_dump($film->duree_minutes);
 
 
-$reponse = $client->request("PUT", "/film/22", [
+// $reponse = $client->request("PUT", "/film/22", [
+//   "form_params" => [
+//           "id_genre" => 2,
+//           "titre"    => "The Double Life of Veronique",
+//           "duree_minutes" => 98
+//       ]
+//   ]);
+
+// $updated = json_decode($reponse->getBody());
+// var_dump($updated);
+
+
+// $reponse = $client->request("GET", "/film/22");
+// $film = json_decode($reponse->getBody());
+// var_dump($film->duree_minutes);
+
+
+
+//// CREATION
+$reponse = $client->request("POST", "/film", [
   "form_params" => [
-          "id_genre" => 2,
-          "titre"    => "The Double Life of Veronique",
-          "duree_minutes" => 98
+          "id_genre" => 18,
+          "titre"    => "Gangsterdam",
+          "duree_minutes" => 341
       ]
   ]);
 
-$updated = json_decode($reponse->getBody());
-var_dump($updated);
+$filmid = json_decode($reponse->getBody());
 
-
-$reponse = $client->request("GET", "/film/22");
+$reponse = $client->request("GET", "/film/".$filmid);
 $film = json_decode($reponse->getBody());
-var_dump($film->duree_minutes);
+var_dump($film);
+
